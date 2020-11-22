bd46e85defccdfb9ca717ba36c43f85cbccb2960
Ruan Herculano <ruannawe95@gmail.com>
2020-11-22 09:48:56 +0000

    Busca, descompactação, converção de dbf para sqlite e execução da migração

---
+++ src/dbf_to_sqlite/main.py
@@ -0,0 +1,22 @@
+import os
+import sys
+from simpledbf import Dbf5
+
+
+def main():
+    file_path = sys.argv[1]
+    dbf = Dbf5(file_path)
+
+    file_name = file_path.split('/')[-1:][0][0:-4]
+    dbf.to_textsql(
+        os.getcwd() + '/' + file_name + '.sql',
+        os.getcwd() + '/' + file_name + '.csv',
+        sqltype='sqlite'
+    )
+
+
+if __name__ == "__main__":
+    if (len(sys.argv) > 2 or len(sys.argv) == 1):
+        raise Error('Invalid number of arguments')
+
+    main()
---
+++ src/migration/README.md
@@ -0,0 +1,1 @@
+### TODO DOC OF LIB HERE
